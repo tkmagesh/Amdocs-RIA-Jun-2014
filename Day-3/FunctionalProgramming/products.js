da879009c608ee42e15f322de43eb80b6abd42e8
tkmagesh <tkmagesh77@gmail.com>
2014-06-25 12:53:42 +0530

    Functional Programming - Implementatinos

--- Day-3/FunctionalProgramming/products.js
+++ Day-3/FunctionalProgramming/products.js
@@ -52,3 +52,95 @@ function productComparerByValue(p1,p2){
 sort(products,productComparerByValue);
 console.log("Products sorted by value...");
 console.table(products);
+
+function min(list,attrName){
+	var result = list[0][attrName];
+	for(var i=1;i<list.length;i++)
+		if (list[i][attrName] < result)
+			result = list[i][attrName];
+	return result;
+}
+
+function max(list,attrName){
+	var result = list[0][attrName];
+	for(var i=1;i<list.length;i++)
+		if (list[i][attrName] > result)
+			result = list[i][attrName];
+	return result;
+}
+
+function sum(list,attrName){
+	var result = 0;
+	for(var i=0;i<list.length;i++)
+		result += list[i][attrName];
+	return result;
+}
+
+function avg(list,attrName){
+	return sum(list,attrName)/list.length;
+}
+
+function filterBy(list,predicate){
+	var result = [];
+	for(var i=0;i<list.length;i++)
+		if (predicate(list[i]))
+			result.push(list[i]);
+	return result;
+}
+
+function countBy(list,predicate){
+	var result = 0;
+	for(var i=0;i<list.length;i++)
+		if (predicate(list[i]))
+			result++;
+	return result;	
+}
+
+function all(list,predicate){
+	for(var i=0;i<list.length;i++)
+		if (!predicate(list[i]))
+			return false;
+	return true;	
+}
+
+function any(list,predicate){
+	for(var i=0;i<list.length;i++)
+		if (predicate(list[i]))
+			return true;
+	return false;	
+}
+
+function groupBy(list,selector){
+	var result = {};
+	var selectorFn = typeof selector === "function" ? selector : function(item){ return item[selector]};
+	for(var i=0;i<list.length;i++){
+		var key = selectorFn(list[i]);
+		result[key] = result[key] || [];
+		result[key].push(list[i]);
+	}
+	return result;
+}
+
+var categories =[
+	{id : 1, name : "grocery"},
+	{id : 2, name : "stationary"}
+]
+
+function join(leftList, rightList, leftAttrName, rightAttrName, transformFn){
+	var result = [];
+	for(var i=0;i<leftList.length;i++){
+		var leftValue = leftList[i][leftAttrName];
+		for(var j=0;j<rightList.length;j++){
+			var rightValue = rightList[j][rightAttrName];
+			if (leftValue === rightValue){
+				result.push(transformFn(leftList[i],rightList[j]));
+			}
+		}
+	}
+	return result;
+}
+
+
+
+
+
