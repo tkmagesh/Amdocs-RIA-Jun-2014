c93f95c9526f18ff1f6a7dfa0c41f576cb6f7b6e
tkmagesh <tkmagesh77@gmail.com>
2014-06-25 11:22:40 +0530

    Case Study - Sorting

--- Day-3/FunctionalProgramming/products.js
+++ Day-3/FunctionalProgramming/products.js
@@ -9,9 +9,46 @@ var products = [
 
 console.table(products);
 
-function sort(){
+function sort(list, attrName){
+	for(var i=0;i<list.length-1;i++)
+		for(var j=i+1;j<list.length;j++)
+			if (list[i][attrName] > list[j][attrName]){
+				var temp = list[i];
+				list[i] = list[j];
+				list[j] = temp;
+			}
+}
+
+sort(products,"id");
+console.log("Products sorted by id...")
+console.table(products);
+
+sort(products,"units");
+console.log("Products sorted by units...")
+console.table(products);
+
+
+var sort = function(list, comparerFn){
+	console.log(comparerFn);
+	for(var i=0;i<list.length-1;i++)
+		for(var j=i+1;j<list.length;j++)
+			if (comparerFn(list[i],list[j]) > 0){
+				var temp = list[i];
+				list[i] = list[j];
+				list[j] = temp;
+			}
+}
+
+function productComparerByValue(p1,p2){
+	var p1Value = p1.units * p1.cost,
+		p2Value = p2.units * p2.cost;
+
+	if (p1Value < p2Value) return -1;
+	if (p1Value === p2Value) return 0;
+	return 1;
 
 }
 
-sort();
+sort(products,productComparerByValue);
+console.log("Products sorted by value...");
 console.table(products);
